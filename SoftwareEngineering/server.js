8af4b93f823dd41288c2f18f2942285944dd1917
lancecorp72 <cs16btech11040@iith.ac.in>
2020-05-07 14:48:53 +0530

    Linked teacher and admin portals to server.js

--- SoftwareEngineering/server.js
+++ SoftwareEngineering/server.js
@@ -7,6 +7,7 @@ var app = express()
 
 const UserDetails = 'UserDetails';
 const StudentPreferences = 'StudentPreferences';
+const TeacherPreferences = 'TeacherPreferences';
 
 var con = mysql.createConnection({
 	host:"localhost",
@@ -137,7 +138,7 @@ app.post('/studentSubmit', urlencodedParser, function(req,res){
 				if(err) throw err;
 				console.log(result2.affectedRows + " record(s) updated");
 			});
-			var sql = "INSERT INTO " + StudentPreferences +" (uname, cgpa, pref) VALUES ('" + req.body.uname + "', " + req.body.cgpa + ", " + "'" + req.body.pref +"')";
+			var sql = "INSERT INTO " + StudentPreferences +" (uname, cgpa, pref) VALUES ('" + req.body.uname + "', " + req.body.cgpa + ", '" + req.body.pref +"')";
 			con.query(sql, function (err, result3) {
 		    	if (err) throw err;
 		  		console.log("1 record inserted");
@@ -150,8 +151,7 @@ app.post('/studentSubmit', urlencodedParser, function(req,res){
 				else{
 					res.writeHead(200,{'Content-Type':'text/html'});
 					res.write(data.toString());
-					
-					}
+				}
 				res.end();
 			});
 			//res.writeHead(403,{'Content-Type':'text/html'});
@@ -160,6 +160,84 @@ app.post('/studentSubmit', urlencodedParser, function(req,res){
 	});
 });
 
+app.post('/teacherSubmit', urlencodedParser, function(req,res){
+	con.query("SELECT * FROM " + UserDetails + " WHERE instname = '" + req.body.instname + "'",function(err,result,fields){
+		if(err)
+			console.log(err);
+		console.log(req.body.instname);
+		if(result.length == 0)
+		{
+			console.log("Instructor not registered");
+			res.writeHead(403,{'Content-Type':'text/html'});
+			res.end('<h1>Instructor not registered</h1>');
+		}
+		else
+		{
+			var sql = "INSERT INTO " + TeacherPreferences + " (cid, instname, nta, pref) VALUES ('" + req.body.cid + "', '" + req.body.instname + "', " + req.body.nta + ", '" + req.body.pref +"')";
+			con.query(sql, function (err, result) {
+		    	if (err) throw err;
+		  		console.log("Record inserted");
+			});
+			fs.readFile('teacherPortal/submitSuccess.html', function(err,data){
+				if(err){
+					console.log(err);
+					res.writeHead(404,{'Content-Type':'text/html'});
+				}
+				else{
+					res.writeHead(200,{'Content-Type':'text/html'});
+					res.write(data.toString());
+				}
+				res.end();
+			});
+		}
+	});
+});
+
+app.post('/adminSubmit', urlencodedParser, function(req,res){
+	con.query("SELECT * FROM " + UserDetails + " WHERE instname = '" + req.body.instname + "'",function(err,result,fields){
+		if(err)
+			console.log(err);
+		console.log(req.body.instname);
+		if(result.length == 0)
+		{
+			console.log("Invalid Admin");
+			res.writeHead(403,{'Content-Type':'text/html'});
+			res.end('<h1>Invalid Admin</h1>');
+		}
+		else
+		{
+			var sql = "";
+			var details = req.body.details.split(",");
+			if(req.body.action=="add")
+				sql = "INSERT INTO ";
+			else if(req.body.action==",modify")
+				sql = "ALTER TABLE ";
+			else
+				sql = "DELETE FROM ";
+
+			if(req.body.table=="teacher")
+				sql += TeacherPreferences + " (cid, instname, nta, pref) VALUES ('" + details[0] + "', '" + details[1] + "', " + details[2] + ", '" + details[3] +"')";
+			else
+				sql += StudentPreferences + " (uname, cgpa, pref) VALUES ('" + details[0] + "', " + details[1] + ", '" + details[2] +"')";
+
+			con.query(sql, function (err, result) {
+		    	if (err) throw err;
+		  		console.log("Action '" + req.body.action + "' completed");
+			});
+			fs.readFile('adminPortal/submitSuccess.html', function(err,data){
+				if(err){
+					console.log(err);
+					res.writeHead(404,{'Content-Type':'text/html'});
+				}
+				else{
+					res.writeHead(200,{'Content-Type':'text/html'});
+					res.write(data.toString());
+				}
+				res.end();
+			});
+		}
+	});
+});
 
 var server = app.listen(8080);
 
