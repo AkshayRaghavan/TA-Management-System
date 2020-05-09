0067a65c281c9913b48ece92cf814e92ec9fb20f
lancecorp72 <cs16btech11040@iith.ac.in>
2020-05-09 19:11:07 +0530

    Enabled running of TA algo as Admin

--- SoftwareEngineering/server.js
+++ SoftwareEngineering/server.js
@@ -194,7 +194,7 @@ app.post('/teacherSubmit', urlencodedParser, function(req,res){
 		    	if (err) throw err;
 		  		console.log("Record inserted");
 			});
-			fs.readFile('teacherPortal/submitSuccess.html', function(err,data){
+			fs.readFile('studentPortal/submitSuccess.html', function(err,data){
 				if(err){
 					console.log(err);
 					res.writeHead(404,{'Content-Type':'text/html'});
@@ -238,7 +238,28 @@ app.post('/adminSubmit', urlencodedParser, function(req,res){
 			});
   		}
 	});
-	fs.readFile('adminPortal/submitSuccess.html', function(err,data){
+	fs.readFile('studentPortal/submitSuccess.html', function(err,data){
+		if(err){
+			console.log(err);
+			res.writeHead(404,{'Content-Type':'text/html'});
+		}
+		else{
+			res.writeHead(200,{'Content-Type':'text/html'});
+			res.write(data.toString());
+		}
+		res.end();
+	});
+});
+
+app.post('/runAlgo', urlencodedParser, function(req,res){
+	let execFile = require('child_process').execFile;
+	execFile('node', ['TA_allocation_algorithm/Multi_round_stable_matching.js'], (err, stdout, stderr) => {
+	    if(err){
+	        console.error('stderr', stderr);
+	        throw err;
+	    }
+	});
+	fs.readFile('studentPortal/submitSuccess.html', function(err,data){
 		if(err){
 			console.log(err);
 			res.writeHead(404,{'Content-Type':'text/html'});
